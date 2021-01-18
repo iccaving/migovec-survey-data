5003ca16cc7e40da225c2eaa2651a33b47aebcd2
Rhys Tyers <rhystyers1@gmail.com>
2021-01-18 13:22:19 +0000

    Add drawing coverage script

--- scripts/create_2d.py
+++ scripts/create_2d.py
@@ -4,107 +4,7 @@ import sys
 import re
 import argparse
 
-# Classes
-
-
-class SourceFile:
-    name = None
-    namespace = []
-
-    def __init__(self, name, namespace=[]):
-        self.name = name
-        self.dirname = dirname(name)
-        self.namespace = namespace.copy()
-
-
-class Survey:
-    source = None
-    sources = []
-    survey_dict = {}
-
-    def __init__(self, source_path):
-        self.source = SourceFile(source_path, [])
-        self.parse(self.source)
-
-    def parse(self, source_file):
-        inputReg = r"(?:\n|^)\s*(?:input|source)\s+\"?([^\s\"]+)?"
-        surveyReg = r"(?:\n|^)\s*survey\s+(\S+)"
-        endSurveyReg = r"(?:\n|^)\s*endsurvey"
-
-        namespace = source_file.namespace.copy()
-        data = ""
-
-        with open(source_file.name, "r") as file:
-            data = file.read()
-
-        for line in data.splitlines():
-            match = re.match(surveyReg, line)
-            if match:
-                namespace.append(match.group(1))
-                self.survey_dict[".".join(namespace)] = source_file.name
-                continue
-
-            match = re.match(endSurveyReg, line)
-            if match:
-                namespace.pop()
-                continue
-
-            match = re.match(inputReg, line)
-            if match:
-                name = abspath(join(source_file.dirname, match.group(1)))
-                new_file = SourceFile(name, namespace)
-                self.sources.append(new_file)
-                self.parse(new_file)
-
-    @staticmethod
-    def convert_from_internal(path):
-        parts = path.split(".")
-        return "{}@{}".format(parts[-1], ".".join(list(reversed(parts[0:-1]))))
-
-    @staticmethod
-    def convert_to_internal(path):
-        parts = path.split("@")
-        if len(parts) == 1:
-            return parts[0]
-        return "{}.{}".format(".".join(list(reversed(parts[1].split(".")))), parts[0])
-
-    @staticmethod
-    def get_survey_name(key):
-        return key.split(".")[-1]
-
-    @staticmethod
-    def get_survey_namespace(key):
-        return ".".join(list(reversed(key.split(".")[0:-1])))
-
-    def get_survey_key(self, name, namespace):
-        if name and namespace:
-            key = "{}.{}".format(".".join(namespace.split(".")[::-1]), name)
-            if key in self.survey_dict:
-                return key
-        else:
-            similair = []
-            for key in self.survey_dict.keys():
-                if name == key:
-                    return key
-                if name in key:
-                    similair.append(key)
-            if len(similair) == 1:
-                return similair[0]
-            elif len(similair) > 1:
-                raise Exception(
-                    "Multiple surveys match that name:\n\t{}".format(
-                        "\n\t".join(self.convert_from_internal(key) for key in similair)
-                    )
-                )
-            else:
-                raise Exception("No survey matches that name")
-        return None
-
-    def get_survey_file(self, key):
-        if key in self.survey_dict:
-            return self.survey_dict[key]
-        return None
-
+from helpers.survey import Survey, SourceFile
 
 # Parse arguments
 
@@ -176,7 +76,6 @@ with open("xvi.thconfig", "w+") as f:
 
 os.system("therion xvi.thconfig")
 os.remove("xvi.thconfig")
-
 # Parse the XVI file
 
 stations = {}
@@ -287,7 +186,7 @@ endscrap"""
 if FORMAT == "plt":
     plt_command = """{type} 0 {x} {y} S{station} P -9 -9 -9 -9"""
 
-    plt_file = """NX D 1 1 1 C{name}.3d\n{points}"""
+    plt_file = """NX D 1 1 1 C{name}.plt\n{points}"""
 
     plt_lines = []
     for line in lines:
