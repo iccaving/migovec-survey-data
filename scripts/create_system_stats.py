658c231650201c25097d0a7d3bfce068be13fb04
Rhys Tyers <rhystyers1@gmail.com>
2021-01-24 20:35:33 +0000

    Improve helper scripts
    Former-commit-id: 4a678df642198f2101d6101250387e5ce750e9e9

--- scripts/create_system_stats.py
+++ scripts/create_system_stats.py
@@ -9,7 +9,8 @@ import multiprocessing as mp
 import json
 import pprint
 
-from helpers.survey import Survey, SourceFile
+from helpers.survey import Survey, SurveyLoader
+from helpers.therion import compile_file, get_stats_from_log
 
 # Parse arguments
 parser = argparse.ArgumentParser(description="Create a skeleton scrap")
@@ -24,86 +25,36 @@ args = parser.parse_args()
 ENTRY_FILE = abspath(args.survey_file)
 
 # Parse survey
-survey = Survey(ENTRY_FILE)
+print("Parsing survey")
+loader = SurveyLoader(ENTRY_FILE)
 
-lengthre = re.compile(r".*Total length of survey legs =\s*(\S+)m")
-depthre = re.compile(r".*Vertical range =\s*(\S+)m")
+total = len(loader.base_surveys)
+counter = mp.Value("i", 0)
 
 
-def get_stats(filepath):
-    config_template = """source {}
-    layout test
-    scale 1 500
-    endlayout
-    """
-    config = config_template.format(filepath)
-    log = ""
+def get_drawn_length(survey):
+    global counter
+    with counter.get_lock():
+        counter.value += 1
+        print("Processing ({}/{}): {}".format(counter.value, total, ".".join(survey.id)))
 
     try:
-        with tempfile.TemporaryDirectory() as tmpdir:
-            config_file = join(tmpdir, "config.thconfig")
-            log_file = join(tmpdir, "log.log")
-            with open(config_file, mode="w+") as tmp:
-                with open(log_file, mode="w+") as tmp2:
-                    tmp.write(config)
-                    tmp.flush()
-                    subprocess.check_output(
-                        args.therion_path + " " + config_file + " -l " + log_file,
-                        shell=True,
-                    )
-                    tmp2.flush()
-                    log = tmp2.read()
-        lenmatch = lengthre.findall(log)
-        depmatch = depthre.findall(log)
-        if len(lenmatch) == 1 and len(depmatch) == 1:
-            return {"length": lenmatch[0], "depth": depmatch[0]}
-    except Exception as e:
-        print(filepath)
-        print(e)
-    return {"length": 0, "depth": 0}
-
-
-drawnre = re.compile(r".*line wall")
-surveyre = re.compile(r".*cent\w\wline")
-
-
-def is_survey(filepath):
-    # We don't want to process the higher level files that combine surveys
-    # because this would duplicate the total length without adding drawn
-    # length.
-    # If the file contains a centreline then it's probably a low level
-    # survey
-    with open(filepath, "r") as tmp:
-        matches = surveyre.findall(tmp.read())
-        return len(matches) > 0
-
-
-def is_drawn(filepath):
-    # If a file contains a wall then it likely has been drawn
-    with open(filepath, "r") as tmp:
-        matches = drawnre.findall(tmp.read())
-        return len(matches) > 0
-
-
-def get_drawn_length(source):
-    if not is_survey(source.name):
+        log = compile_file(survey.file_path, therion_path=args.therion_path)
+    except:
+        print("\tFailed")
         return None
-    th2s = [s for s in survey.sources if s.dirname == source.dirname and s.type == "th2"]
-    print("Processing: {}".format(source.name))
-    length = float(get_stats(source.name)["length"])
-    plan_th2 = next((x for x in th2s if x.projection == "plan"), None)
-    extended_th2 = next((x for x in th2s if x.projection == "extended"), None)
+    stats = get_stats_from_log(log)
     proj_is_drawn = {
-        "plan": plan_th2 and is_drawn(plan_th2.name),
-        "extended": extended_th2 and is_drawn(extended_th2.name),
+        "plan": any([s.is_drawn() for s in survey.scraps if s.projection == "plan"]),
+        "extended": any([s.is_drawn() for s in survey.scraps if s.projection == "extended"]),
     }
-    return (length, proj_is_drawn, source)
+    length = float(stats["length"])
+    return (length, proj_is_drawn, survey)
 
 
-th_sources = [s for s in survey.sources if s.type == "th"]
-
+print("Compiling survey files")
 pool = mp.Pool(mp.cpu_count())
-results = pool.map(get_drawn_length, th_sources)
+results = pool.map(get_drawn_length, loader.base_surveys)
 
 systems = [
     "system_migovec",
@@ -148,14 +99,18 @@ for result in results:
         need_extended.append(source.name)
 
 for system in systems:
-    filepath = survey.get_survey_file(survey.get_survey_key(system, "system_migovec" if system != "system_migovec" else ""))
-    stats = get_stats(filepath)
+    print(system)
+    file_path = loader.get_survey_by_id(system).file_path
+    try:
+        log = compile_file(survey.file_path, therion_path=args.therion_path)
+    except Exception as e:
+        print(e)
+        continue
+    stats = get_stats_from_log(log)
     length = float(stats["length"])
     length_km = round(length / 1000, 1)
     depth = round(float(stats["depth"]))
     json_data[system]["stats"] = {"length": length, "length_km": length_km, "depth": depth}
-
-# json_data["system_migovec"]["stats"] = {"length": round(float(get_length(survey.source.name)) / 1000, 1)}
 
 print("Need plan:")
 pprint.pprint(need_plan)
